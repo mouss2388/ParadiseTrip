627ff4695e4a1e4e91651ffa2fc943d95fa6e831
mouss2388 <mouss2388@gmail.com>
2018-11-06 20:50:23 +0100

    Ajout: + Creation travellers Activity + travellers Layout

---
+++ app/src/main/java/com/abdelouahad/mustapha/myapp/TravellersActivity.java
@@ -0,0 +1,42 @@
+package com.abdelouahad.mustapha.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+
+public class TravellersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travellers);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_eco_class:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.radio_premium_class:
+                if (checked)
+                    // Ninjas rule
+                    break;
+
+            case R.id.radio_business_class:
+                if (checked)
+                    // Ninjas rule
+                    break;
+
+            case R.id.radio_first_class:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
+    }
+}
