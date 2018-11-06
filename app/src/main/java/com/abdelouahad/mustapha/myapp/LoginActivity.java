7cbba76110f4119bb0d6c2aaf479da0258097370
mouss2388 <mouss2388@gmail.com>
2018-11-06 16:03:49 +0100

    Ajout: + Mode portrait +Creation ActivityLogin et activity_login

---
+++ app/src/main/java/com/abdelouahad/mustapha/myapp/LoginActivity.java
@@ -0,0 +1,74 @@
+package com.abdelouahad.mustapha.myapp;
+
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9#_~!$&'()*+,;=:.\"(),:;<>@\\[\\]\\\\]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*$";
+    private Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+    private Matcher matcher;
+
+
+    TextInputLayout passwordWrapper =null;
+    TextInputLayout usernameWrapper =null;
+    Button btn = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)  {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        passwordWrapper =  findViewById(R.id.passwordWrapper);
+        usernameWrapper=  findViewById(R.id.emailWrapper);
+        btn = findViewById(R.id.btnLogin);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameWrapper.getEditText().getText().toString();
+                String password = passwordWrapper.getEditText().getText().toString();
+                Toast.makeText(LoginActivity.this,
+                        username +":"+password,
+                        Toast.LENGTH_LONG).show();
+
+
+                if (!validateEmail(username)) {
+                    usernameWrapper.setError("Not a valid email address!");
+                } else if (!validatePassword(password)) {
+                    passwordWrapper.setError("Not a valid password!");
+                } else {
+                    usernameWrapper.setErrorEnabled(false);
+                    passwordWrapper.setErrorEnabled(false);
+                    doLogin();
+                }
+            }
+        });
+
+    }
+
+    public void doLogin() {
+        Toast.makeText(getApplicationContext(), "OK! I'm performing login.", Toast.LENGTH_SHORT).show();
+        // login procedure; not within the scope of this tutorial.
+    }
+
+    public boolean validateEmail(String email) {
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public boolean validatePassword(String password) {
+        return password.length() > 5;
+    }
+
+}
+
