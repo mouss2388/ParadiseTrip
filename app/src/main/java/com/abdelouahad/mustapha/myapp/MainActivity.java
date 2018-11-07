634d5458a95269d45c561d7bd6928d63f0e63a6c
mouss2388 <mouss2388@gmail.com>
2018-11-07 12:50:02 +0100

    + StatusBar color en noir + New color pour toolbar, hearder_nav, item_selected + Style modifié

--- app/src/main/java/com/abdelouahad/mustapha/myapp/MainActivity.java
+++ app/src/main/java/com/abdelouahad/mustapha/myapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.abdelouahad.mustapha.myapp;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,6 +15,8 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -27,6 +31,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
 
         //crée la gallerie des destinations tendence
